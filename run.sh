32b2d8de0733018e4e3482d6913aaf8ed5c1cd3f
FarnHua <alhena.farn@gmail.com>
2023-02-11 15:01:54 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,24 @@
-for i in 1 1000 10000 100000
+for i in 1e-4 
 do
     CUDA_VISIBLE_DEVICES=0 python main.py \
                         --mode finetune \
                         --prompt GPT2 \
+			--model gpt2-medium\
                         --bot DialogGPT \
                         --type bias \
-                        --exp_name bias-ppo2-seed${i} \
+                        --exp_name bias-ppo2-inlr${i}-2 \
                         --log_interval 25 \
-                        --seed ${i} \
+                        --seed 1 \
                         --bz 8 \
                         --k_epoch 5 \
                         --discount_r 0.98 \
-                        --end_batch 800 \
-                        --sample_time 1 \
+                        --end_batch 1000 \
+                        --sample_time 2 \
                         --max_pt_len 10 \
-                        --tags seed \
+                        --tags inner-lr \
+			--inner_lr ${i}\
                         --init_step 2 \
-                        --save_path bias-ppo2-seed${i} \
+                        --save_path bias-ppo2-inlr${i}-2\
                         --save_interval 20 \
-                        --wandb enabled
+                        --wandb online
 done
