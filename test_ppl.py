f492dcff21f5e5dbf05de06f2ca3241a3d6794d2
FarnHua <alhena.farn@gmail.com>
2023-02-14 15:39:58 +0800

    test_ppl

---
+++ test_ppl.py
@@ -0,0 +1,26 @@
+import numpy as np
+import torch
+import random
+import argparse
+import importlib
+
+from argparse import Namespace
+from torch.utils.data import DataLoader
+from os.path import join
+
+
+input = "What"
+
+bot1 = importlib.import_module(".module","bots.gpt2").bot
+
+bot2 = importlib.import_module(".module","bot.gpt2").bot
+bot2.lm.load_state_dict(torch.load('./gpt2_finetune/gpt2-medium-4.pt'))
+bot2.lm.eval()
+
+
+output1 = bot1.make_response(input)
+output2 = bot2.make_response(input)
+
+print(output1, output2)
+
+
