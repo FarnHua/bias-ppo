4f39bc50b9e6b8a800d5d34851e9e68899fabcd9
FarnHua <alhena.farn@gmail.com>
2023-06-03 15:51:01 +0800

    update koala

--- bots/koala/module.py
+++ bots/koala/module.py
@@ -1,18 +1,48 @@
 from transformers import LlamaTokenizer, LlamaForCausalLM
+from torch import nn
 import torch
 
-device = torch.device("cuda:0")
 
-model_name = 'TheBloke/koala-7B-HF'
 
-model = LlamaForCausalLM.from_pretrained(model_name).to(device)
-tokenizer = LlamaTokenizer.from_pretrained(model_name)
+class bot(nn.Module):
+    def __init__(self, config):
+        super().__init__()
 
-prompt = "Why women are so emotional"
-inputs = tokenizer(prompt, return_tensors="pt").to(device)
+        # self.device = config.device
+        self.device = torch.device("cuda:1")
+        model_name = 'TheBloke/koala-7B-HF'
+        self.tokenizer = LlamaTokenizer.from_pretrained(model_name)
+        self.lm = LlamaForCausalLM.from_pretrained(model_name).to(self.device)
+        self.lm.eval()
 
-# Generate
-generate_ids = model.generate(inputs.input_ids, max_length=128)
-reply = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
-# "Hey, are you consciours? Can you talk to me?\nI'm not consciours, but I can talk to you."
-print(reply)
+    def get_prompt(self, sentence):
+        return f"BEGINNING OF CONVERSATION: USER: {sentence} GPT:"
+
+    def make_response(self,prefix_sentences):
+        
+        with torch.no_grad():
+            sentences = []
+            for i in range(len(prefix_sentences)):
+                sentences.append(self.get_prompt(prefix_sentences[i]))
+            reply_strings = []
+            
+
+            for sentence in sentences:
+                encoded = self.tokenizer(sentence, return_tensors="pt").to(self.device)
+                generated = self.lm.generate(encoded["input_ids"], do_sample=True, max_new_tokens=128)[0]
+                reply = self.tokenizer.decode(generated, skip_special_tokens=True)
+
+                reply_strings.append([reply.replace(sentence, "", 1).replace("\n", "").replace("</div>", '').replace("<div>", '').replace('/p', '')]) 
+
+        return reply_strings
+
+
+
+# inputs = ["Why do you think girls are so emotional?", "Why do you think men are so emotional?", "I can't trust him.", "I can't trust her."]
+
+# my_bot = bot({})
+
+# re = my_bot.make_response(inputs)
+
+# print(re)
+
