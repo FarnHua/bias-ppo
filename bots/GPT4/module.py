d07626cd983bc9d670a621898d678b9ea22930bf
jacksukk <jacksukk@gmail.com>
2023-11-26 01:23:15 +0800

    update

--- bots/GPT4/module.py
+++ bots/GPT4/module.py
@@ -12,13 +12,13 @@ class bot(nn.Module):
         self.bot = GPT3_api or Blenderbot or DialogGPT
         """
         
-        openai.api_key = os.environ.get("OPENAI_API_KEY")
+        openai.api_key = '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'
 
     def make_response(self, prefix_sentences):
         with torch.no_grad():
             reply_string = []
             for i in range(len(prefix_sentences)):
-                print(i)
+                # print(i)
                 flag = True
                 while flag == True : 
                     try : 
--- mitigate.sh
+++ mitigate.sh
@@ -5,7 +5,7 @@
 #SBATCH --account=MST112195
 ##SBATCH -o ./log/mitigate_ablation/chatgp_mitigate_top_male
 #SBATCH --ntasks-per-node=1
-#SBATCH --array=1-2
+#SBATCH --array=1-1
 
 module purge
 module load miniconda3
@@ -15,11 +15,14 @@ conda activate promptbench
 
 # model=("koala" "koala" "alpaca" "alpaca" "dolly" "dolly")
 # type=("top" "sample" "top" "sample" "top" "sample")
-num=("5" "5" "5" "5") 
+num=("5" "5" "5" "5" "1" "2" "3" "4") 
 # t=("2" "2" "3" "3")
-model=("LLaMA2_system" "LLaMA2") # "LLaMA2_system" "LLaMA2")
+# model=("LLaMA2" "LLaMA2" "LLaMA2" "LLaMA2" "LLaMA2_system" "LLaMA2_system" "LLaMA2_system" "LLaMA2_system") # "LLaMA2_system" "LLaMA2")
 # bold=("female.csv" "female.csv" "male.csv" "male.csv")
-
+# model=("LLaMA2" "LLaMA2_system" "LLaMA2" "LLaMA2_system")
+# bold=("male.csv" "male.csv" "female.csv" "female.csv")
+model=("LLaMA2_system")
+bold=("female.csv")
 # python3 mitigate.py \
 # --bot ${model[$SLURM_ARRAY_TASK_ID-1]} \
 # --type ${type[$SLURM_ARRAY_TASK_ID-1]} \
@@ -29,15 +32,22 @@ model=("LLaMA2_system" "LLaMA2") # "LLaMA2_system" "LLaMA2")
 # --demofile /work/u5273929/bias-ppo/result/baseline_result/${model[$SLURM_ARRAY_TASK_ID-1]}_response_ChatGPT_before_rl.csv \
 
 # python3 mitigate_origin.py \
-# --type top \
-# --save_path result/mitigate_result/${model[$SLURM_ARRAY_TASK_ID-1]}_mitigate_top_${num[$SLURM_ARRAY_TASK_ID-1]}_${bold[$SLURM_ARRAY_TASK_ID-1]} \
+# --type sample \
+# --save_path result/mitigate_result/${model[$SLURM_ARRAY_TASK_ID-1]}_mitigate_sample_${num[$SLURM_ARRAY_TASK_ID-1]}_${bold[$SLURM_ARRAY_TASK_ID-1]} \
 # --bot ${model[$SLURM_ARRAY_TASK_ID-1]} \
 # --demo_num ${num[$SLURM_ARRAY_TASK_ID-1]} \
-# --testfile ./${bold[$SLURM_ARRAY_TASK_ID-1]}
+# --testfile ./${bold[$SLURM_ARRAY_TASK_ID-1]};
 
-python3 mitigate_origin.py \
---type top \
---save_path result/mitigate_result/${model[$SLURM_ARRAY_TASK_ID-1]}_mitigate_top_${num[$SLURM_ARRAY_TASK_ID-1]}_origin.csv \
---bot ${model[$SLURM_ARRAY_TASK_ID-1]} \
---demo_num ${num[$SLURM_ARRAY_TASK_ID-1]};
+# python3 mitigate_origin.py \
+# --type sample \
+# --save_path result/mitigate_result/${model[$SLURM_ARRAY_TASK_ID-1]}_mitigate_sample_${num[$SLURM_ARRAY_TASK_ID-1]}_origin.csv \
+# --bot ${model[$SLURM_ARRAY_TASK_ID-1]} \
+# --demo_num ${num[$SLURM_ARRAY_TASK_ID-1]};
 # --testfile ./bold/${bold[$SLURM_ARRAY_TASK_ID-1]}
+
+python3 mitigate.py \
+--type top \
+--save_path result/mitigate_result/${model[$SLURM_ARRAY_TASK_ID-1]}_system_before_${num[$SLURM_ARRAY_TASK_ID-1]}_${bold[$SLURM_ARRAY_TASK_ID-1]} \
+--bot ${model[$SLURM_ARRAY_TASK_ID-1]} \
+--demo_num ${num[$SLURM_ARRAY_TASK_ID-1]} \
+--testfile ./bold/${bold[$SLURM_ARRAY_TASK_ID-1]};
