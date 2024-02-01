e4a6f6c21d95e56370ef12e2b038d83d87fc2a60
Hua Farn <72127075+FarnHua@users.noreply.github.com>
2024-02-02 02:28:36 +0800

    Update module.py

--- bots/GPT4/module.py
+++ bots/GPT4/module.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from openai import OpenAI
 import openai
 import time
 import os
@@ -11,29 +12,32 @@ class bot(nn.Module):
         """
         self.bot = GPT3_api or Blenderbot or DialogGPT
         """
-        
-        openai.api_key = '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'
+
+       
+        openai.api_key = os.environ.get("OPENAI_API_KEY")
+        self.client = OpenAI()
 
     def make_response(self, prefix_sentences):
         with torch.no_grad():
             reply_string = []
             for i in range(len(prefix_sentences)):
-                # print(i)
                 flag = True
                 while flag == True : 
                     try : 
-                        reply = openai.ChatCompletion.create(
+                        reply = self.client.chat.completions.create(
                             model="gpt-4-0613",
                             messages=[
-                                {"role": "system", "content" : "Please respond the given sentence without as an language model."}, 
+                                {"role": "system", "content" : "Please act as a human and give a human-like response."}, 
                                 {"role": "user", "content": prefix_sentences[i]}
                             ],
+                            max_tokens=128
                         )
-                        tmp = reply['choices'][0]['message']['content']
+                        tmp = reply.choices[0].message.content
 
-                        reply_string.append([reply['choices'][0]['message']['content']])
+                        reply_string.append([tmp])
                         flag = False
                     except : 
                         print("Here to sleep 1 second.")
                         time.sleep(1)
         return reply_string
+
